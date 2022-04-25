7ac4c97bd9d7153bde55778b875307243a85189e
devopsbychandu <79252166+devopsbychandu@users.noreply.github.com>
2022-04-25 18:41:37 +0530

    Update gitsync.sh

--- gitsync.sh
+++ gitsync.sh
@@ -1,4 +1,5 @@
 git remote -v
 git remote rm origin
-git remote add origin https://devopsbychandu:Chandu@380@github.com/devopsbychandu/sync3.git
+git remote add origin https://github.com/devopsbychandu/sync3.git
 git remote -v
+git push https://devopsbychandu:Chandu@380@github.com/devopsbychandu/sync3.git --all
