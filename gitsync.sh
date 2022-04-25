79d7427783b65bfdc66ab2e5914a52c246a7c384
devopsbychandu <79252166+devopsbychandu@users.noreply.github.com>
2022-04-25 18:27:19 +0530

    Update gitsync.sh

--- gitsync.sh
+++ gitsync.sh
@@ -1,6 +1,2 @@
 git remote -v
 cd jenkins
-git remote rm origin
-git remote add origin https://github.com/devopsbychandu/sync3.git
-git remote -v
-git push https://github.com/devopsbychandu/sync3.git '*:*'
